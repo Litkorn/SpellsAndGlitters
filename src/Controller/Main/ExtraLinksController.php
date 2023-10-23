49fd82e60ea9266c0571dcce54bef209d172f0c3
celine L <c.leibenguth.77@gmail.com>
2023-10-24 01:05:01 +0200

    rework ( templates and controller ): better files organisation

---
+++ src/Controller/Main/ExtraLinksController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Main;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExtraLinksController extends AbstractController
+{
+    #[Route('/CGU', name: 'app_cgu')]
+    public function index(): Response
+    {
+        return $this->render('Main/extra_links/index.html.twig', [
+            'title' => 'Conditions Générales d\'Utilisation',
+            'controller_name' => 'CGU'
+        ]);
+    }
+}
