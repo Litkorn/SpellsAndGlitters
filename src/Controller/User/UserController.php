20a5c3ff2c9119893707b096bec177f8fbeb424f
celine L <celine.leibenguth.67@gmail.com>
2023-11-29 18:06:48 +0100

    feat (info): verification if user exists and is the same as the one from passed in url (which we are trying to access to)

--- src/Controller/User/UserController.php
+++ src/Controller/User/UserController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller\User;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\SecurityRequestAttributes;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/profil', name:'app_profil_')]
 class UserController extends AbstractController
@@ -15,14 +19,20 @@ class UserController extends AbstractController
         return $this->render('User/dashboard.html.twig');
     }
 
-    #[Route('/informations', name: 'informations')]
-    public function manageInfos()
+    #[Route('/informations/{id}', name: 'informations')]
+    public function manageInfos(Request $request, UserRepository $userRepo, User $user = null)
     {
-        return $this->render('User/info.html.twig');
+        if($user == null || $userRepo->isSameUser($request, $user) == false){
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('User/info.html.twig', [
+            'user'  => $user
+        ]);
     }
 
     #[Route('/favorites', name:'favorites')]
-    public function manageFavorites()
+    public function manageFavorites(Request $request, User $user = null)
     {
         return $this->render('User/favorites.html.twig');
     }
